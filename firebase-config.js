c31e05d9fca96597b7d21367864c7f38e2c0da51
cloudclique <stanislav.zhukov@pm.me>
2025-12-05 17:56:42 +0100

    dsa

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,21 @@
-// firebase-config.js
+// --- 1. Firebase Configuration and Initialization ---
 
 const firebaseConfig = {
     apiKey: "AIzaSyDzVVtxCN1xvAZGZYG-Ydke08SYZEYNIlc",
     authDomain: "my-gk-collection.firebaseapp.com",
     databaseURL: "https://my-gk-collection-default-rtdb.firebaseio.com",
     projectId: "my-gk-collection",
-    storageBucket: "my-gk-collection.appspot.com",
+    storageBucket: "my-gk-collection.firebasestorage.app",
     messagingSenderId: "880192992279",
     appId: "1:880192992279:web:53c9e9e8992699a254d852"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-const auth = firebase.auth();
-const storage = firebase.storage();
+const db = app.firestore();
+const auth = app.auth();
+const collectionName = "items";
 
-export { app, auth, db, storage };
+// Export services and constants
+export { app, auth, db, collectionName };
+
+
